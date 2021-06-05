3181e629f0b76ce36a6c4c04310097cda8546f37
Joon Park <aisgandy@hotmail.com>
2021-06-05 16:28:42 +0900

    Finished Xylophone iOS Project

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 import AVFoundation
 
@@ -22,7 +14,7 @@ class ViewController: UIViewController {
         playSound(soundName: sender.currentTitle!)
         sender.alpha = 0.5
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            sender.alpha = 1
+            sender.alpha = 1.0
         }
     }
     
